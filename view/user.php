2f5ef722b7445802b12d755513d04eb0c40ee11f
Jose Demarzo <demarzomonterojose@gmail.com>
2022-10-17 22:23:53 -0400

    local libs

--- view/user.php
+++ view/user.php
@@ -186,6 +186,6 @@
 
     <?php include('templates/footer.php'); ?>
     <script src="view/js/validate.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script src="view/lib/bootstrap.js"></script>
 </body>
 </html>
